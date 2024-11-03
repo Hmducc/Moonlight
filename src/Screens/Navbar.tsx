394145fd4ab8417b50d85c25773dfe4230054c6f
Hmducc <hmd.2k3@gmail.com>
2024-11-03 18:21:25 +0700

    final

--- src/Screens/Navbar.tsx
+++ src/Screens/Navbar.tsx
@@ -64,15 +64,11 @@ export default function Example() {
           className="lg:hidden"
         >
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 manrope right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
+                <img alt="" src={logo} className="h-8 w-auto" />
               </a>
               <button
                 type="button"
